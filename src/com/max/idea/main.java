5320e7a9abfff5f29a19f2a1fa665d14cdf04ac4
Anna <isebella@mail.ru>
2020-12-01 20:42:26 +0300

    Второе задание вынесено в отдельную функцию. Доавлено третье задание

--- src/com/max/idea/main.java
+++ src/com/max/idea/main.java
@@ -2,9 +2,28 @@ package com.max.idea;
 
 public class main {
     public static void main(String[] args) {
+        lesson2();
+        lesson3();
+    }
+
+    public static void lesson2(){
         double a = 5.856;
         byte b = 127;
         System.out.println(a);
         System.out.println(b);
+    }
+
+    public static void lesson3(){
+        int[] nums = {1, 2, 3, 4, 5};
+        int temp = nums[0];
+        nums[0] = nums[nums.length - 1];
+        nums[nums.length - 1]= temp;
+
+        for(int i = 0; i < nums.length; i++){
+            System.out.println(nums[i]);
+        }
+
+        int summ = nums[0] + nums[nums.length / 2];
+        System.out.println(summ);
     }
 }
